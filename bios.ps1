f10ad480fafb6a2f6820221b554c7eb94bae2ada
bourama1 <bourama1@uhk.cz>
2023-11-29 09:47:45 +0100

    change wmic output

--- bios.ps1
+++ bios.ps1
@@ -1,7 +1,21 @@
 # Get BIOS information using Get-CimInstance
 $bios = Get-CimInstance -ClassName Win32_BIOS
-$jsonFilePath = "bios.json"
-$bios | ConvertTo-Json | Out-File -FilePath $jsonFilePath
 
-# Compare
-python .\compareJSON.py
+# Get the current date and time in a specific format
+$currentDateTime = Get-Date -Format "yyyyMMdd-HHmmss"
+
+# Construct the output file name with the datetime
+$outputFileName = "BIOS-$currentDateTime.txt"
+
+# Create a string to store the output
+$outputString = "BIOS Information:`n-----------------`n"
+
+foreach ($property in $bios.PSObject.Properties) {
+    $outputString += "$($property.Name): $($property.Value)`n"
+    Write-Host "$($property.Name): $($property.Value)"
+}
+
+# Save the output to a file
+$outputString | Out-File -FilePath $outputFileName
+
+Write-Host "Output saved to $outputFileName"
