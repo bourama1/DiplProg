4fb17f4f8db6994429bb2e73a7e6a245ff163c24
bourama1 <bourama1@uhk.cz>
2023-11-29 15:02:49 +0100

    add waiting for winaudit to finish

--- winAudit.ps1
+++ winAudit.ps1
@@ -1,5 +1,5 @@
-# Create 
-.\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArCOHG /f=audit.html /T=datetime
+# Start WinAudit.exe and wait for it to exit
+Start-Process -FilePath ".\WinAudit.exe" -ArgumentList "/r=gsoPxuTUeERNtnzDaIbMpmidcSArCOHG", "/f=audit.html", "/T=datetime" -Wait
 
 # Compare
 python .\compare.py
