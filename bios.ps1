05947dc2429db375a647546e4d20cfc1e213cedd
bourama1 <bourama1@uhk.cz>
2024-01-22 16:06:53 +0100

    Add file compare to bios script

--- bios.ps1
+++ bios.ps1
@@ -1,3 +1,15 @@
+Function Get-FileName($initialDirectory)
+{  
+ [System.Reflection.Assembly]::LoadWithPartialName(“System.windows.forms”) |
+ Out-Null
+
+ $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+ $OpenFileDialog.initialDirectory = $initialDirectory
+ $OpenFileDialog.filter = “All files (*.*)| *.*”
+ $OpenFileDialog.ShowDialog() | Out-Null
+ $OpenFileDialog.filename
+} #end function Get-FileName
+
 # Get BIOS information using Get-CimInstance
 $bios = Get-CimInstance -ClassName Win32_BIOS
 
@@ -12,10 +24,39 @@ $outputString = "BIOS Information:`n-----------------`n"
 
 foreach ($property in $bios.PSObject.Properties) {
     $outputString += "$($property.Name): $($property.Value)`n"
-    Write-Host "$($property.Name): $($property.Value)"
 }
 
 # Save the output to a file
 $outputString | Out-File -FilePath $outputFileName
 
 Write-Host "Output saved to $outputFileName"
+
+### File compare
+$firstFilePath = Get-FileName("D:\Documents\DiplProg")
+
+# Kontrola, zda uživatel vybral soubory
+if ($firstFilePath) {
+    # Načtení obsahu souborů
+    $firstFileContent = Get-Content -Path $firstFilePath
+    $secondFileContent = Get-Content -Path $outputFileName
+
+    # Porovnání obsahu souborů
+    $differences = Compare-Object $firstFileContent $secondFileContent
+
+    # Výpis rozdílů
+    if ($differences) {
+        Write-Host "Nalezeny rozdíly mezi soubory:"
+        $differences | ForEach-Object {
+            $message = if ($_.SideIndicator -eq "<=") {
+                "První soubor obsahuje: $_"
+            } else {
+                "Druhý soubor obsahuje: $_"
+            }
+            Write-Host $message
+        }
+    } else {
+        Write-Host "Soubory jsou identické."
+    }
+} else {
+    Write-Host "Výběr souborů byl zrušen."
+}
