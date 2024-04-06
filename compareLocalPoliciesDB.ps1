5c514223110e1b57e5b2c8499c743e9b7ebcdea0
bourama1 <bourama1@uhk.cz>
2024-04-06 21:13:41 +0200

    add comparing of LocalPolicies

--- compareLocalPoliciesDB.ps1
+++ compareLocalPoliciesDB.ps1
@@ -9,3 +9,46 @@ if (-not $databasePath) {
   Write-Host "Vyberte databázi:"
   $databasePath = GetFileName("D:\Documents\DiplProg")
 }
+
+# SQL dotaz pro vybrání dat z tabulky
+$sqlQuery1 = @"
+SELECT * FROM LocalPolicies
+WHERE Audit_ID = (SELECT MAX(Audit_ID) FROM LocalPolicies)
+"@
+
+$sqlQuery2 = @"
+SELECT * FROM LocalPolicies
+WHERE Audit_ID = (SELECT MAX(Audit_ID) FROM LocalPolicies WHERE Audit_ID < (SELECT MAX(Audit_ID) FROM LocalPolicies))
+"@
+
+# Zjisteni jestli jiz existuje tabulka rozdily, popr. jeji smazani
+$tableName = "RozdilyPolicies"
+if (TableExists $tableName $databasePath) {
+  DropTable $tableName $databasePath
+}
+
+# Vytvoření tabulky v nové databázi pro uložení rozdílů
+$createQuery = @"
+CREATE TABLE $tableName (
+  ID AUTOINCREMENT PRIMARY KEY,
+  Audit_ID INT,
+  [Name] TEXT(255),
+  [Value] TEXT(255)
+)
+"@
+ExecuteQuery -databasePath $databasePath -sqlQuery $createQuery
+
+# Vykonání dotazu pro obě databáze
+$data1 = ExecuteQuery -databasePath $databasePath -sqlQuery $sqlQuery1
+$data2 = ExecuteQuery -databasePath $databasePath -sqlQuery $sqlQuery2
+
+# Porovnání dat
+$differences = Compare-Object -ReferenceObject $data1 -DifferenceObject $data2 -Property "Name", "Value" -PassThru
+
+# Uložení rozdílů
+foreach ($diff in $differences) {
+  $insertDiffQuery = @"
+INSERT INTO $tableName (Audit_ID, [Name], [Value]) VALUES ('$($diff.Audit_ID)', '$($diff.Name)', '$($diff.Value)')
+"@
+  ExecuteQuery -databasePath $databasePath -sqlQuery $insertDiffQuery
+}
